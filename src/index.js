0991b90148aa575d0608beaea3d09c6c1057d271
mactavishkkk <isaiasleite230@gmail.com>
2022-07-06 12:12:38 -0400

    upload to archives

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import "core-js"
 import "regenerator-runtime/runtime"
 
+import Sentry from "./sentry"
+import { getSentry } from "./sentry_service"
+
+async function loadSentrys() {
+    let sentrys = [];
+    let sentrysJSON = await getSentry();
+    sentrysJSON.forEach(element => {
+        const newSentry = new Sentry(element["sentryId"], element["fullname"], element["year_range_min"], element["year_range_max"]);
+        sentrys.push(newSentry);
+    });
+    renderSentry(sentrys);
+}
+
+async function renderSentry(sentry) {
+    const olElement = document.getElementById("sentry-list")
+    sentry.forEach(element => {
+        const liElement = document.createElement("li")
+        const text = `ID: ${element.id} | NOME: ${element.name} | Risco de Colisão ente: ${element.yearMinColision} e ${element.yearMaxColision}`
+        liElement.innerText = text
+        olElement.appendChild(liElement);
+    })
+}
+
+loadSentrys();
---
+++ src/sentry.js
@@ -0,0 +1,8 @@
+export default class Sentry{
+    constructor(sentryId, name, yearMinColision, yearMaxColision){
+        this.sentryId = sentryId,
+        this.name = name,
+        this.yearMinColision = yearMinColision,
+        this.yearMaxColision = yearMaxColision
+    }
+}
